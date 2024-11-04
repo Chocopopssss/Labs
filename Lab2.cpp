729483fabbc75ed6cc1e0c8c3d7020b15d467cd2
Chocopopssss <chlenveli555@gmail.com>
2024-11-05 01:08:26 +0300

    Лабораторная работа №2. Вариант 5.
    Лабораторная работа №2. Вариант 5.

---
+++ Lab2.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a > 0 && c < 10 * 10 * 10 * 10 && b)
+    {
+        cout << a * b * c;
+    }
+    else
+    {
+        cout << "Неправильные стороны.";
+    }
+
+    return 0;
+}
+
