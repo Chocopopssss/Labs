c4e17b0474c1f40fe33f0a4dd2388bf350a4167d
Chocopopssss <chlenveli555@gmail.com>
2024-11-23 01:38:58 +0300

    Лабораторная работа №3, Вариант 5

---
+++ Lab3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    if (0 < x < 10 * 10 * 10 * 10 * 10 * 10 * 10 * 10 * 10)
+    {
+        int i = 2;
+        cout << (x | (0 << i));
+    }
+
+    return 0;
+}
+
